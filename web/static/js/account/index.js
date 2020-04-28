7599d2c04c6ce9e1e989309ca2b02cab9ed99651
Bruce <1046244623@qq.com>
2020-04-28 15:21:34 +0800

    删除和恢复账号

---
+++ web/static/js/account/index.js
@@ -0,0 +1,54 @@
+;
+var account_index_ops = {
+    init:function(){
+        this.eventBind()
+    },
+    eventBind:function(){
+        $(".remove").click(function(){
+            id = $(this).attr("data")
+            $.ajax({
+                url:common_ops.buildUrl("/account/remove-or-recover"),
+                type:"POST",
+                data:{
+                    "id":id,
+                    "acts":"remove"
+                },
+                dataType:"json",
+                success:function(resp){
+                    console.log(resp.msg)
+                    if (resp.code == 200) {
+                        window.location.href = common_ops.buildUrl("/account/index")
+                    }
+                },
+                error:function(error){
+                    console.log(error)
+                }
+            })
+        })
+
+        $(".recover").click(function(){
+            id = $(this).attr("data")
+            $.ajax({
+                url:common_ops.buildUrl("/account/remove-or-recover"),
+                type:"POST",
+                data:{
+                    "id":id,
+                    "acts":"recover"
+                },
+                dataType:"json",
+                success:function(resp){
+                    console.log(resp.msg)
+                    if (resp.code == 200) {
+                        window.location.href = common_ops.buildUrl("/account/index")
+                    }
+                },
+                error:function(error){
+                    console.log(error)
+                }
+            })
+        })
+    }
+}
+$(document).ready(function(){
+    account_index_ops.init()
+})
