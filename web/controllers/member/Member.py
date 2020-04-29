94b7e7ef66b426153160703b3479e438351daddd
Bruce <1046244623@qq.com>
2020-04-29 14:10:40 +0800

    会员列表数据库

--- web/controllers/member/Member.py
+++ web/controllers/member/Member.py
@@ -7,3 +7,15 @@ router_member = Blueprint("member_page",__name__)
 @router_member.route("/index")
 def index():
     return ops_render("member/index.html")
+
+@router_member.route("/info")
+def info():
+    return ops_render("member/info.html")
+
+@router_member.route("/set")
+def set():
+    return ops_render("member/set.html")
+
+@router_member.route("/comment")
+def comment():
+    return ops_render("member/comment.html")
